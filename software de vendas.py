b69a1fb1004df0c257aff55c34df20c1ff6f7097
Amanda <amanda525911@gmail.com>
2023-02-07 11:02:47 -0300

    up final

--- software de vendas.py
+++ software de vendas.py
@@ -1,24 +1,12 @@
 #funcionamento do software em python
 
+qtd = 0
+tecla = "c"
+total = 0.0
 
-p1 = float(input("Digite um produto: "))
-p2 = float(input("Digite um produto: "))
-p3 = float(input("Digite um produto: "))
-p4 = float(input("Digite um produto: "))
-p5 = float(input("Digite um produto: "))
-soma = (p1+p2+p3+p4+p5)
-pagamento = float(input("Qual é a forma de pagamento? "))
-
-
-print ("Soma: ", soma)
-print ("Forma de pagamento: ", pagamento)
-
-
-if pagamento == "dinheiro":
-    print("total a pagar: ", soma, "troco: ", troco)
-elif pagamento == "cartão débito":
-    print("insira seu cartão e digite a senha.", soma)
-    print("pagamento autorizado")
-elif pagamento == "cartão crédito":
-    print("insira seu cartão e digite a senha.", soma)
-    print("pagamento autorizado")
+while (qtd <=10 and tecla!="p"):
+    preço = float(input("Preço: "))
+    total = total + preço
+    qtd = qtd + 1
+    tecla = input("pressione qualquer tecla para continuar ou 'p' para parar...")
+print("O total da compra é ", total)
